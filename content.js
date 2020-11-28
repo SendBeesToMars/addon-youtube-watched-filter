45bb7d8ff517b446c213eacc949024f01e308294
Winmillion <datum1337@gmail.com>
2020-11-28 04:20:03 +0000

    cleanup

--- content.js
+++ content.js
@@ -2,7 +2,6 @@
 
 let flag = false;
 function checkNode(){
-    // var recomendations = document.getElementById("secondary");
     var watchedVideos = document.getElementsByTagName("ytd-thumbnail-overlay-resume-playback-renderer");
     if(watchedVideos.length === 0){
         // sleeps for 1s if node not found
@@ -14,7 +13,6 @@ function checkNode(){
 
     // deletes watched videos :)
     for (let index = 0; index < len; index++) {
-        console.log("index: "+ index + "   " + len);
         // array is always at 0 as remove() removes item from array
         watchedVideos[0].closest("ytd-compact-video-renderer").remove();
  
