7f44bc7d0b1652c4ea129ff3b21bb0c1471cb97c
Winmillion <datum1337@gmail.com>
2020-11-28 03:30:47 +0000

    works pretty fast now :)

--- content.js
+++ content.js
@@ -1,51 +1,29 @@
 
-const callback = () => {
-    var watchedVideos = document.getElementsByTagName("ytd-thumbnail-overlay-resume-playback-renderer");
-    chrome.storage.sync.get("range", function(value){
-        let range = value.range;
-        // removes the up next video if watched
-            // console.log(watchedVideos[0].closest("ytd-compact-video-renderer"));
-            // console.log(watchedVideo[0].closest("ytd-compact-autoplay-renderer"));
-        if (typeof watchedVideos[0] !== "undefined" ) {
-            if (typeof watchedVideos[0].closest("ytd-compact-autoplay-renderer") !== "undefined" && watchedVideos[0].closest("ytd-compact-autoplay-renderer" !== null)) {
-                // watchedVideo[0].parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-                // vid.remove();
-                watchedVideo[0].closest("ytd-compact-autoplay-renderer").remove();
-                console.log("1");
-                }
-            else if (typeof watchedVideos[0].closest("ytd-compact-video-renderer") !== "undefined") {
-                // watchedVideo[0].parentElement.parentElement.parentElement.parentElement.remove();
-                // vid.remove();
-                watchedVideos[0].closest("ytd-compact-video-renderer").remove();
-                console.log("2");
-            }
-            else {
-                console.log("3");
-            }
-        }
-    });
-};
 
-
+let flag = false;
 function checkNode(){
     // var recomendations = document.getElementById("secondary");
     var watchedVideos = document.getElementsByTagName("ytd-thumbnail-overlay-resume-playback-renderer");
     if(watchedVideos.length === 0){
-        console.log("node not found");
-        window.setTimeout(checkNode, 500);
+        // sleeps for 1s if node not found
+        window.setTimeout(checkNode, 1000);
         return;
     }
-    console.log("node found");
-    console.log(watchedVideos.length);
-    console.log(watchedVideos);
-    const config = { attributes: true, childList: true, subtree: true };
-    const observer = new MutationObserver(callback);
-    var recomendations = document.getElementById("secondary");
-    observer.observe(recomendations, config);
+
+    let len = watchedVideos.length;
+
+    // deletes watched videos :)
+    for (let index = 0; index < len; index++) {
+        console.log("index: "+ index + "   " + len);
+        // array is always at 0 as remove() removes item from array
+        watchedVideos[0].closest("ytd-compact-video-renderer").remove();
+ 
+    }
+    // checks again after 2.5s
+    if (!flag) {
+        flag = true;
+        window.setTimeout(checkNode, 2500);
+    }
 }
 
 checkNode();
-
-browser.runtime.connect().onDisconnect.addListener(function() {
-    observer.disconnect();
-});
