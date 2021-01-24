14508f2fe6e1699e3ae58f0bcd4e088223654fbf
Winmillion <datum1337@gmail.com>
2021-01-24 17:31:06 +0000

    removed unecessary loops

--- content.js
+++ content.js
@@ -6,41 +6,43 @@ let filteredVideosNumInitial = 0;
 chrome.storage.sync.get("filtered", (data) => {
     filteredVideosNum = data.filtered;
     filteredVideosNumInitial = data.filtered;
-    
 });
+
+let video_index = 0;
 
 function checkVideos(){
     var watchedVideos = document.getElementsByTagName("ytd-thumbnail-overlay-resume-playback-renderer");
+
+    // if no watched videos
     if(watchedVideos.length === 0){
         return;
     }
     
-    if (watchedVideos[0].closest("ytd-compact-video-renderer") == null){
+    try {
+        current_video = watchedVideos[video_index].closest("ytd-compact-video-renderer");
+        video_title = current_video.querySelector("#video-title").textContent.trim();
+    } catch (error) {
+        // video does not exist or has been removed
         return;
     }
 
-    let len = watchedVideos.length;
-    console.log(len);
+    // gets the % of the video watched
+    let watched_ratio = watchedVideos[video_index].childNodes[1].style.width.split("%")[0];
 
-    // deletes watched videos :)
-    let index = 0;
+    // gets user selected range and checks % of video watched
+    chrome.storage.sync.get("range", (data) => {
+        // removes watched video if suits range
+        if (parseInt(watched_ratio) <= parseInt(data.range)){
+            console.log("removing", video_title, ":", watched_ratio, "vs", data.range);
+            current_video.remove();
+            filteredVideosNum++;
+        }
+        else{
+            video_index++;
+        }
+    });
 
-    for (let i = 0; i < len; i++) {
-        // gets the % of the video watched
-        let watched_ratio = watchedVideos[index].childNodes[1].style.width.split("%")[0];
-        chrome.storage.sync.get("range", (data) => {
-            console.log("index " + watched_ratio + "   " + data.range);
-            if (parseInt(watched_ratio) <= parseInt(data.range)){
-                console.log("3");
-                // array index at 0 as remove() removes item from array
-                watchedVideos[index].closest("ytd-compact-video-renderer").remove();
-                filteredVideosNum++;
-            }
-            else{
-                index++;
-            }
-        });
-    }
+    // increments filtered videos number
     if(filteredVideosNumInitial != filteredVideosNum){
         chrome.storage.sync.set({
             "filtered": filteredVideosNum
@@ -53,7 +55,7 @@ const observerConfig = { attributes: false, childList: true, subtree: true };
 const sidebarObserver = new MutationObserver(checkVideos);
 
 
-// addon does not work while traversing youtube. Addoan thinks it one page, thus the need to observer changes
+// youtube uses a different way to load its pages thus the need to check constantly
 function checkSidebar(){
     const sidebar = document.getElementById("secondary");
     // if sidebar not loaded, try agian in .5s
